c8f9055b9a01256265fc9a511da1b48a3ac926be
On4r4p <on4r4p@PinkyPie.teamrocket.org>
2021-10-13 16:46:32 +0200

    fix chrm/sbit/trns/srgb/off+newtitle

--- Chunklate.py
+++ Chunklate.py
@@ -148,7 +148,7 @@ def Summarise(infos,Summary_Footer=False):
                   infos = tmp
 
          filename = folder+"Summary_Of_"+os.path.splitext(os.path.basename(FILE_Origin))[0]
-         print(Candy("Color","green","-Saving Summary : "),"**hided**")
+         print(Candy("Color","green","-Saving Summary : "),filename)
          with open(filename,'a+') as f:
 
              if Summary_Header is True:
